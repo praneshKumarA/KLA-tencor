87e61a35e175a247cd3d6bb172a08d8f843a96cf
Pranesh Kumar A <53888235+praneshKumarA@users.noreply.github.com>
2022-03-08 12:41:52 +0530

    Update kla_1.py

--- kla_1.py
+++ kla_1.py
@@ -2,22 +2,23 @@ import yaml
 import datetime
 import time
 
-with open('Milestone1\Milestone1A.yaml') as f:
+with open('Milestone1\Milestone1B.yaml') as f:
     mil_1a = yaml.load(f, Loader = yaml.FullLoader)
 
 def task_func(tasks, log, i):
     with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
-        log1 = log[:len(log) - 7] + '.' + str(i) + ' Entry' + '\n'
+        log1 = str(datetime.datetime.now()) + log[log.index(';'):len(log) - 7] + '.' + str(i) + ' Entry' + '\n'
         f.write(log1)
-        log2 = log1[:len(log) - 7] + '.' + str(i) + ' Executing ' + str(tasks[i]['Function']) + '(' + str(tasks[i]['Inputs']['FunctionInput']) + ', ' + str(tasks[i]['Inputs']['ExecutionTime']) + ')' + '\n'
+        time.sleep(int(tasks[i]['Inputs']['ExecutionTime']))
+        log2 = str(datetime.datetime.now()) + log1[log1.index(';'):len(log) - 7] + '.' + str(i) + ' Executing ' + str(tasks[i]['Function']) + '(' + str(tasks[i]['Inputs']['FunctionInput']) + ', ' + str(tasks[i]['Inputs']['ExecutionTime']) + ')' + '\n'
         f.write(log2)
-        log3 = log[:len(log) - 7] + '.' + str(i) + ' Exit' + '\n'
+        log3 = str(datetime.datetime.now()) + log[log.index(';'):len(log) - 7] + '.' + str(i) + ' Exit' + '\n'
         f.write(log3)
         f.close()
 
 def flow_func(tasks, log, i):
     with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
-        log1 = log[:len(log) - 7] + '.' + str(i) + ' Entry' + '\n'
+        log1 = str(datetime.datetime.now()) + log[log.index(';'):len(log) - 7] + '.' + str(i) + ' Entry' + '\n'
         f.write(log1)
         f.close()
     for x in tasks[i]['Activities']:
@@ -26,7 +27,7 @@ def flow_func(tasks, log, i):
         if tasks[i]['Activities'][x]['Type'] == 'Flow':
             flow_func(tasks[i]['Activities'], log1, x)
     with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
-        log2 = log[:len(log) - 7] + '.' + str(i) + ' Exit' + '\n'
+        log2 = str(datetime.datetime.now()) + log[log.index(';'):len(log) - 7] + '.' + str(i) + ' Exit' + '\n'
         f.write(log2)
         f.close()
             
@@ -45,7 +46,7 @@ def milestone1(data):
             flow_func(activities, log, i)
             
     with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
-        log_exit = log[:len(log) - 7] + ' Exit'
+        log_exit = str(datetime.datetime.now()) + log[log.index(';'):len(log) - 7] + ' Exit'
         f.write(log_exit)
         f.close()
                 
