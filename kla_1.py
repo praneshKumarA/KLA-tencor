eb5f1a133ee1a94b918d3d7727cd13d0406a08e5
Pranesh Kumar A <53888235+praneshKumarA@users.noreply.github.com>
2022-03-08 12:16:15 +0530

    Update kla_1.py

--- kla_1.py
+++ kla_1.py
@@ -9,29 +9,45 @@ def task_func(tasks, log, i):
     with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
         log1 = log[:len(log) - 7] + '.' + str(i) + ' Entry' + '\n'
         f.write(log1)
-        log2 = log1[:len(log) - 7] + ' Executing ' + str(tasks[i]['Function']) + '(' + str(tasks[i]['Inputs']['FunctionInput']) + ', ' + str(tasks[i]['Inputs']['ExecutionTime']) + ')' + '\n'
+        log2 = log1[:len(log) - 7] + '.' + str(i) + ' Executing ' + str(tasks[i]['Function']) + '(' + str(tasks[i]['Inputs']['FunctionInput']) + ', ' + str(tasks[i]['Inputs']['ExecutionTime']) + ')' + '\n'
         f.write(log2)
         log3 = log[:len(log) - 7] + '.' + str(i) + ' Exit' + '\n'
         f.write(log3)
         f.close()
 
 def flow_func(tasks, log, i):
-    
-
+    with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
+        log1 = log[:len(log) - 7] + '.' + str(i) + ' Entry' + '\n'
+        f.write(log1)
+        f.close()
+    for x in tasks[i]['Activities']:
+        if tasks[i]['Activities'][x]['Type'] == 'Task':
+            task_func(tasks[i]['Activities'], log1, x)
+        if tasks[i]['Activities'][x]['Type'] == 'Flow':
+            flow_func(tasks[i]['Activities'], log1, x)
+    with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
+        log2 = log[:len(log) - 7] + '.' + str(i) + ' Exit' + '\n'
+        f.write(log2)
+        f.close()
+            
 def milestone1(data):
     with open('Log_mil1A.txt', 'w', encoding = 'UTF8') as f:
         log = str(datetime.datetime.now()) + ';' + str(list(data)[0]) + ' ' + str('Entry') + '\n'
         f.write(log)
         f.close()
         
-        activities = data[str(list(data)[0])]['Activities']
-        for i in activities:
-            if activities[i]['Type'] == 'Task':
-                task_func(activities, log, i)
+    activities = data[str(list(data)[0])]['Activities']
+    for i in activities:
+        if activities[i]['Type'] == 'Task':
+            task_func(activities, log, i)
         
-            if activities[i]['Type'] == 'Flow':
-                
-                for x in activities[i]:
+        if activities[i]['Type'] == 'Flow':
+            flow_func(activities, log, i)
+            
+    with open('Log_mil1A.txt', 'a', encoding = 'UTF8') as f:
+        log_exit = log[:len(log) - 7] + ' Exit'
+        f.write(log_exit)
+        f.close()
                 
 print(milestone1(mil_1a))
     
